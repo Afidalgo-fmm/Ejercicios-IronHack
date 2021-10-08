ad80fef46d56d4553670e0b0f3438513851f5ce6
Afidalgo-fmm <73072170+Afidalgo-fmm@users.noreply.github.com>
2021-10-08 12:27:37 +0200

    Update ej_iterativos.py

--- ej_iterativos.py
+++ ej_iterativos.py
@@ -10,3 +10,26 @@ Write a simple for loop to print integers from 10 to 30. Also write a code to pr
 1.3 Write a code to print this pattern. Modify the print statement in the dummy code provided
 
 '''
+#1.1
+
+for i in range(10,30):
+    print(i)
+
+for i in range(10,30,3):
+    print(i)
+
+#1.2
+
+for i in range(10,30):
+    if i % 5 ==0:
+        print(i)
+
+#1.3 no he sido capaz de sacarlo
+for i in range(1, 6, 2):
+    print(i*'*')
+
+#1.4 no he sido capaz de sacarlo solo
+# space = 2
+for i in range(1,6,2):
+    print(space*" ",i*"*")
+    space = space - 1
